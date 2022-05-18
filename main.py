ee69a0a246c9e57b75890d1c0bdc19855182f948
andrejcermak <andrej.cermak@vacuumlabs.com>
2022-05-18 16:13:39 +0200

    Add second function

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def print_hi(name):
 def first_function():
     print("this is first function")
 
+def second_function():
+    print("this is second function")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
