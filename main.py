6107af436b2339192d3aef405d59d0e9d4745837
andrejcermak <andrej.cermak@vacuumlabs.com>
2022-05-18 16:16:45 +0200

    Finish feature2

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def sum(a, b):
+    print(a+b)
+
+def mul(a, b):
+    print(a*b)
+
+def div(a, b):
+    print(a/b)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+# Adding another docs
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
