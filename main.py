560d3992922834ad1a266d332772a3733fec9b0f
andrejcermak <andrej.cermak@vacuumlabs.com>
2022-05-19 13:23:03 +0200

    Add fibbonacci call to main

--- main.py
+++ main.py
@@ -33,5 +33,5 @@ def fib(a):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    print(fib(5))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
